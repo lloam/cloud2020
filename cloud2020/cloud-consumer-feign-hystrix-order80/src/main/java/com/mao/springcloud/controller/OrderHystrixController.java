55b8f8f5075abf0b2a7dc0d571fc9572bd6037f7
lloam <2196143404@qq.com>
2021-09-18 11:32:48 +0800

    :art:客户端服务降级

--- cloud2020/cloud-consumer-feign-hystrix-order80/src/main/java/com/mao/springcloud/controller/OrderHystrixController.java
+++ cloud2020/cloud-consumer-feign-hystrix-order80/src/main/java/com/mao/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package com.mao.springcloud.controller;
 
 import com.mao.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,11 +30,19 @@ public class OrderHystrixController {
         return result;
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutFallbackMethod", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+    })
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
     public String paymentInfo_Timeout(@PathVariable("id") Integer id)
     {
+        int age = 10/0;
         String result = paymentHystrixService.paymentInfo_Timeout(id);
         log.info("*****result："+result);
         return result;
+    }
+
+    public String paymentInfo_TimeoutFallbackMethod(@PathVariable("id") Integer id) {
+        return "我是消费者80，对方支付繁忙，请 10 秒后再试或自己运行出错检查自己，/(ㄒoㄒ)/~~";
     }
 }
