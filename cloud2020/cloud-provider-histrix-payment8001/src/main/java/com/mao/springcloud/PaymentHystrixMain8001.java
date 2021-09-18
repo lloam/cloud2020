1cb05731ef4ceacafd18fc14a2e1109d54863198
lloam <2196143404@qq.com>
2021-09-18 11:01:29 +0800

    :art:服务端服务降级

--- cloud2020/cloud-provider-histrix-payment8001/src/main/java/com/mao/springcloud/PaymentHystrixMain8001.java
+++ cloud2020/cloud-provider-histrix-payment8001/src/main/java/com/mao/springcloud/PaymentHystrixMain8001.java
@@ -2,6 +2,7 @@ package com.mao.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class, args);
--- cloud2020/cloud-provider-histrix-payment8001/src/main/java/com/mao/springcloud/service/PaymentService.java
+++ cloud2020/cloud-provider-histrix-payment8001/src/main/java/com/mao/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.mao.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -24,14 +26,26 @@ public class PaymentService
     }
 
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler", commandProperties = {
+            // 设置线程请求超时时间为 3 秒
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String paymentInfo_Timeout(Integer id)
     {
-        int timeNumber = 3;
+        int timeNumber = 5;
+        // int age = 10/0; // 模拟出错
         try {
+            System.out.println("未降级前服务线程名为：" + Thread.currentThread().getName());
             TimeUnit.SECONDS.sleep(timeNumber);
         } catch (InterruptedException e) {
             e.printStackTrace();
+            System.out.println("出错后服务线程名为：" + Thread.currentThread().getName());
         }
         return "线程池："+Thread.currentThread().getName()+"  paymentInfo_Timeout，id"+id+"\t"+"o(n_n)o哈哈~"+"  耗时 "+timeNumber+" 秒钟";
+    }
+
+
+    public String paymentInfo_TimeoutHandler(Integer id) {
+        return "线程池： " + Thread.currentThread().getName() + "  系统繁忙或者运行报错，请稍后再试，id：" + id + "\t" +  "/(ㄒoㄒ)/~~";
     }
 }
