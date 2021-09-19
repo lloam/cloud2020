f007d18da725e124bde12579910e0b6f027ab1c5
lloam <2196143404@qq.com>
2021-09-19 23:22:17 +0800

    :art:SpringCloud Config服务端配置

---
+++ cloud2020/cloud-config-server-3344/src/main/java/com/mao/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.mao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Author: lloam
+ * Date: 2021/9/19 22:03
+ * Description: 配置中心主启动类
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
