21949ec23ef4916fe56b984d20315ef475f50a20
lloam <2196143404@qq.com>
2021-09-19 23:51:49 +0800

    :art:config客户端配置

--- cloud2020/cloud-config-client-3355/src/main/java/com/mao/springcloud/ConfigClientMain3355.java
+++ cloud2020/cloud-config-client-3355/src/main/java/com/mao/springcloud/ConfigClientMain3355.java
@@ -2,19 +2,18 @@ package com.mao.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Author: lloam
- * Date: 2021/9/19 22:03
- * Description: 配置中心主启动类
+ * Date: 2021/9/19 23:38
+ * Description:
+ * config 客户端配置主启动
  */
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfigCenterMain3344 {
+public class ConfigClientMain3355 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCenterMain3344.class, args);
+        SpringApplication.run(ConfigClientMain3355.class, args);
     }
 }
---
+++ cloud2020/cloud-config-client-3355/src/main/java/com/mao/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.mao.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: lloam
+ * Date: 2021/9/19 23:43
+ * Description:
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
