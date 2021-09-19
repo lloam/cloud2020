5ab2f36a3636253326000c25fb5c96f823325c60
lloam <2196143404@qq.com>
2021-09-19 16:41:56 +0800

    :art:网关gateway

---
+++ cloud2020/cloud-gateway-gateway9527/src/main/java/com/mao/springcloud/GatewayMain9527.java
@@ -0,0 +1,18 @@
+package com.mao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Author: lloam
+ * Date: 2021/9/19 16:14
+ * Description: 网关主启动
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
