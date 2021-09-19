df3422dd581a9fdaeb3d8a980906a0c247b3baed
lloam <2196143404@qq.com>
2021-09-19 18:09:40 +0800

    :art:常用Predicate断言

---
+++ cloud2020/cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * Author: lloam
+ * Date: 2021/9/19 17:39
+ * Description:
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zdt = ZonedDateTime.now();
+        System.out.println(zdt);
+        // 2021-09-19T17:40:21.871+08:00[Asia/Shanghai]
+    }
+}
