cdcc03082c94863143e1cd8be636502061572da8
lloam <2196143404@qq.com>
2021-09-21 21:54:00 +0800

    :art:steam消息消费者

---
+++ cloud2020/cloud-stream-rabbitmq-consumer8802/src/main/java/com/mao/springcloud/StreamMQMain8802.java
@@ -0,0 +1,16 @@
+package com.mao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: lloam
+ * Date: 2021/9/21 21:39
+ * Description: 主启动
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
