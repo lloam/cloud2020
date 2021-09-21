25f4a633f5b49319824a4232764783e492f77a81
lloam <2196143404@qq.com>
2021-09-21 22:28:16 +0800

    :art:stream消息消费者2

---
+++ cloud2020/cloud-stream-rabbitmq-consumer8803/src/main/java/com/mao/springcloud/StreamMQMain8803.java
@@ -0,0 +1,16 @@
+package com.mao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: lloam
+ * Date: 2021/9/21 22:15
+ * Description: 主启动
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
---
+++ cloud2020/cloud-stream-rabbitmq-consumer8803/src/main/java/com/mao/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,28 @@
+package com.mao.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * Author: lloam
+ * Date: 2021/9/21 22:16
+ * Description:
+ */
+@Component
+@EnableBinding(Sink.class)
+@Slf4j
+public class ReceiveMessageListenerController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        log.info("消费者 1 号 -----> 接受到的消息：" + message.getPayload() + "\t  port：" + serverPort);
+    }
+}
